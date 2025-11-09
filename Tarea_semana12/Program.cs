c9a15b3d10ac314203ec01e00d6308d23130c729
jeremyx <jeremy.jsolorzano@gmail.com>
2025-11-08 19:24:59 -0500

    subiendo en la rama main mi nombre

---
+++ Tarea_semana12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_semana12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mi nombre es Jeremy Angel Juan de Dios Solorzano.");
+        }
+    }
+}
